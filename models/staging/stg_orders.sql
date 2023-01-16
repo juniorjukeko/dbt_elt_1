92b9d61afbb7355781aae2f4af49def02be402d2
Iswara Jay <junior.jukeko@gmail.com>
2023-01-16 14:30:56 +0000

    demo dbt

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,12 @@
+WITH ORDERS AS (
+
+    SELECT
+        "ID" AS ORDER_ID,
+        "USER_ID" AS CUSTOMER_ID,
+        "ORDER_DATE",
+        "STATUS"
+
+    FROM RAW_DATA.ORDERS
+)
+
+SELECT * FROM ORDERS
